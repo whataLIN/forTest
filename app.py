b5e34810bd9bd7aa23b2ccab68d9287e5567000d
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-03-27 14:32:57 +0900

    키값

--- app.py
+++ app.py
@@ -353,12 +353,12 @@ elif choice == "시뮬레이션":
         total_stats=6
         while total_stats <= 40:
             # player["name"] = st.text_input(f"Enter the name of player {i+1}")
-            player["Shooting"] = st.slider(f"player {i+1}의 슈팅 능력치", min_value=1, max_value=10, value=1)
-            player["Dribbling"] = st.slider(f"player {i+1}의 드리블 능력치", min_value=1, max_value=10, value=1)
-            player["Passing"] = st.slider(f"player {i+1}의 패스 능력치", min_value=1, max_value=10, value=1)
-            player["Rebounding"] = st.slider(f"player {i+1}의 리바운드 능력치", min_value=1, max_value=10, value=1)
-            player["Defense"] = st.slider(f"player {i+1}의 수비 능력치", min_value=1, max_value=10, value=1)
-            player["Stamina"] = st.slider(f"player {i+1}의 스테미나 능력치", min_value=1, max_value=10, value=1)
+            player["Shooting"] = st.slider(f"슈팅", min_value=1, max_value=10, value=1, key=f"shooting_{i}")
+            player["Dribbling"] = st.slider(f"드리블", min_value=1, max_value=10, value=1, key=f"Dribbling_{i}")
+            player["Passing"] = st.slider(f"패스", min_value=1, max_value=10, value=1), key=f"Passing_{i}"
+            player["Rebounding"] = st.slider(f"리바운드", min_value=1, max_value=10, value=1, key=f"Rebounding_{i}")
+            player["Defense"] = st.slider(f"수비", min_value=1, max_value=10, value=1, key=f"Defense_{i}")
+            player["Stamina"] = st.slider(f"스테미나", min_value=1, max_value=10, value=1, key=f"Stamina_{i}")
 
             total_stats=player["Shooting"]+player["Dribbling"]+player["Passing"]+player["Rebounding"]+player["Defense"]+player["Stamina"]
             if total_stats > 40:
