842c987d8557915381a7164f311d853d1d013e82
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-03-28 16:58:22 +0900

    inplace

--- app.py
+++ app.py
@@ -515,7 +515,10 @@ elif choice == "시뮬레이션":
     teaminfo['SEED']='Missed Tournament'
     teaminfo['WAB']=0
 
-    df = df.reindex(index=["CONF", 'ADJOE', 'ADJDE', 'BARTHAG', 'EFG_O', 'EFG_D', 'TOR', 'TORD', 'ORB', 'DRB', 'FTR', 'FTRD', '2P_O', '2P_D', '3P_O', '3P_D', 'ADJ_T', 'WAB', 'POSTSEASON', 'SEED'])
+    df = df.reindex(index=
+        ["CONF", 'ADJOE', 'ADJDE', 'BARTHAG', 'EFG_O', 'EFG_D', 
+        'TOR', 'TORD', 'ORB', 'DRB', 'FTR', 'FTRD', '2P_O', '2P_D', 
+        '3P_O', '3P_D', 'ADJ_T', 'WAB', 'POSTSEASON', 'SEED'], inplace=True)
 
 
     st.write(teaminfo)
